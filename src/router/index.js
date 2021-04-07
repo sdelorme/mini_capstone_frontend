65acf107e0ed6d94549d9f270be81cd4b71fc153
Seth Delorme <delorme.s.m@gmail.com>
2021-04-07 18:45:42 -0500

    refactors app.vue and index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import ProductsIndex from "../views/ProductsIndex.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/products",
+    name: "Products",
+    component: ProductsIndex,
   },
   {
     path: "/about",
