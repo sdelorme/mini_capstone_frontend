b15008a3466196b6742a22f316571b08f2204e69
Seth Delorme <delorme.s.m@gmail.com>
2021-04-07 18:41:42 -0500

    adds index action to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products" :key="product.id">
+      <h2>{{ product.name }}</h2>
+      <p>Description: {{ product.description }}</p>
+      <p>Price: {{ product.price }}</p>
+      <hr />
+    </div>
   </div>
 </template>
 
 <style></style>
 <script>
-// import axios from "axios";
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("getting index");
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
